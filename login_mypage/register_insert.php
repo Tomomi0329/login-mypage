15866f90dbf073c84d5522c3f7f2ecf37053dd23
Tomomi0329 <41553212work@gmail.com>
2023-01-27 10:34:34 +0900

    PHP課題　マイページへのログイン

---
+++ login_mypage/register_insert.php
@@ -0,0 +1,23 @@
+﻿<?php
+mb_internal_encoding("utf8");
+
+//DB接続
+$pdo = new PDO("mysql:dbname=lesson1;host=localhost;","root","");
+
+$stmt = $pdo->prepare("insert into login_mypage(name,mail,password,picture,comments)values(?,?,?,?,?)");
+
+$stmt->execute();
+
+$stmt->bindValue(1,$_POST['name']);
+$stmt->bindValue(2,$_POST['mail']);
+$stmt->bindValue(3,$_POST['password']);
+$stmt->bindValue(4,$_POST['path_filename']);
+$stmt->bindValue(5,$_POST['comments']);
+
+
+$stmt->execute();
+$pdo = NULL;
+
+header('Location:after_register.html')
+
+?>
